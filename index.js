c4b4fae224a3e9fc6fcc0d3988887bf330d747aa
Vivek <viveksharmaa139@gmail.com>
2026-02-13 21:37:45 +0530

    some other changes

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-const express = require('express');
-const userRouter = require('./Router/userRoutes')
-const connectDB = require('./DB/UserDb')
+const express = require("express");
+const userRouter = require("./Router/userRoutes");
+const connectDB = require("./DB/UserDb");
 const app = express();
-const cors = require('cors');
-const dotenv = require('dotenv');
-const cookieParser = require('cookie-parser');
+const cors = require("cors");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
 dotenv.config();
 connectDB();
-app.use(cors(
-  {
-    origin: 'http://localhost:5173',
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://your-frontend-domain.com"],
     credentials: true,
-  }
-));
+  }),
+);
+
 app.use(cookieParser());
 app.use(express.json());
 
 // Sample route
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
-app.use('/users', userRouter);
+app.use("/users", userRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
